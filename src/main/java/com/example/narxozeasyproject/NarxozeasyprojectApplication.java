6519fdb7d3e424ca30753e068aa55d41041e3072
icdfh <madiyar.jetibergen@narxoz.kz>
2024-02-08 10:16:26 +0600

    first commit

---
+++ src/main/java/com/example/narxozeasyproject/NarxozeasyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.narxozeasyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarxozeasyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarxozeasyprojectApplication.class, args);
+	}
+
+}
